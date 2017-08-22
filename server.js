71f3d1b0af09e39d30bb4efcc66b634beb146995
Banda Montes, Luis MiguelX <luis.miguelx.banda.montes@intel.com>
2017-08-22 16:54:18 -0500

    Rename holaBot.js to server.js and add env library

---
+++ server.js
@@ -0,0 +1,64 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+require('dotenv').config()
+
+// Get secrets from server environment
+var botConnectorOptions = { 
+    appId: process.env.MICROSOFT_APP_ID, 
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+};
+
+// Create bot
+var connector = new builder.ChatConnector(botConnectorOptions);
+var bot = new builder.UniversalBot(connector);
+
+bot.dialog('/', function (session) {
+    
+    //respond with user's message
+    session.send("You said " + session.message.text);
+});
+
+// Setup Restify Server
+var server = restify.createServer();
+
+// Handle Bot Framework messages
+server.post('/api/messages', connector.listen());
+
+// Serve a static web page
+// server.get(/.*/, restify.serveStatic({
+// 	'directory': '.',
+// 	'default': 'index.html'
+// }));
+
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url); 
+});
+/*var restify = require('restify');
+var builder = require('botbuilder');
+
+// Levantar restify
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// No te preocupes por estas credenciales por ahora, luego las usaremos para conectar los canales.
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID, 
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+// Ahora utilizamos un UniversalBot
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+// Dialogos
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.text(session, '¿Cómo te llamas?');
+    },
+    function (session, results) {
+        let msj = results.response;
+        session.send(`Hola ${msj}!`);
+    }
+]);*/
