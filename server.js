2d0ded3c957cb049173b6fefc41eaf2504ad3fc6
Banda Montes, Luis MiguelX <luis.miguelx.banda.montes@intel.com>
2017-08-22 17:02:49 -0500

    Fixed get to a statical page

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ var server = restify.createServer();
 server.post('/api/messages', connector.listen());
 
 // Serve a static web page
-// server.get(/.*/, restify.serveStatic({
-// 	'directory': '.',
-// 	'default': 'index.html'
-// }));
+server.get('/', restify.plugins.serveStatic({
+ directory: __dirname,
+ default: '/index.html'
+}));
 
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
